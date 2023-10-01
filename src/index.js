e57dc0d82b3a42d3146637901c1a30ec336aa478
kunaldec022002 <kunalingale72@gmail.com>
2023-10-01 23:25:43 +0530

    added externel css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+//destructuring process
+function Greeting({name,city})
+{
+
+  return(
+    <div className="greeting-card">
+      <h1>Hello, {name}</h1>
+      <p>How is whether at {city}</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+
+  <Greeting name="Kunal" city="Talegaon"/>
+  <Greeting name="kundan" city="Wardha"/>
+  <Greeting name="Sanika" city="Amaravti"/>
+  <Greeting name="Shamali" city="pune"/>
+  </>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
